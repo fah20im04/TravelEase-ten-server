cbb8aac608887f10f2a3641acd91b56814438179
Fahim Ahmed Ayon <fa369383@gmail.com>
2025-11-16 08:21:11 -0800

    done mongo connection

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
+
+let db, vehiclesCollection, usersCollection, bookingCollection;
+
+async function connectDB() {
+  try {
+    const client = new MongoClient(uri, {
+      serverApi: { version: ServerApiVersion.v1 },
+      tls: true,
+    });
+
+    console.log("Connecting to MongoDB...");
+    await client.connect();
+    db = client.db(dbName);
+    vehiclesCollection = db.collection("vehicles");
+    usersCollection = db.collection("users");
+    bookingCollection = db.collection("bookings");
+    console.log(" Connected to MongoDB:", db.databaseName);
+  } catch (err) {
+    console.error(" MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+}
+
+connectDB();
+
+
 app.get("/", (req, res) => res.send("TravelEase server running..."));
 
 app.listen(port, () => console.log(` Server running on port ${port}`));
